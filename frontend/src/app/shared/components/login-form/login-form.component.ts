1ef849089b3f60630ddcebb0442fb35fbc500537
valeriinikolaichuk <powerlift.rv@gmail.com>
2026-05-01 19:10:31 +0200

    install Jest

--- frontend/src/app/shared/components/login-form/login-form.component.ts
+++ frontend/src/app/shared/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-form',
@@ -12,7 +13,35 @@ export class LoginFormComponent {
   password = '';
   language = 'en';
 
+  constructor(private http: HttpClient) {}
+
   onSubmit() {
-    console.log(this.login, this.password, this.language);
+    const loginData = {
+      login: this.login,
+      password: this.password,
+      language: this.language,
+    };
+
+    this.http.post('/api/login', loginData, {
+      withCredentials: true,
+    }).subscribe({
+      next: (res: any) => {
+        this.checkAndRoute(res);
+      },
+      error: () => {
+        console.error('error by path: /api/login');
+      },
+    });
+  }
+
+  checkAndRoute(json: any) {
+    console.log('SERVER RESPONSE:', json);
+
+    if (json.success) {
+      alert('LOGIN SUCCESS');
+      // тут потім буде router.navigate або popup.close()
+    } else {
+      alert('LOGIN FAILED');
+    }
   }
 }
